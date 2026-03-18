9ea1e773d23ecb0d84eb95391c5b64e45b2194e8
Sköthagen Nordstrand <john.s1@utb.hudiksvall.se>
2026-03-18 08:56:47 +0100

    added fullscreen button to menus

--- uno-ultimate-ai-simulator/autoloads/global_inputs.gd
+++ uno-ultimate-ai-simulator/autoloads/global_inputs.gd
@@ -1,5 +1,7 @@
 extends Node
 
+signal window_mode_changed(is_fullscreen: bool)
+
 func _ready():
 	toggle_window_mode()
 
@@ -15,26 +17,23 @@ func toggle_window_mode():
 	var is_currently_full = DisplayServer.window_get_mode(main_id) != DisplayServer.WINDOW_MODE_WINDOWED
 	
 	if not is_currently_full:
-		# GÅ TILL "FALSK" FULLSCREEN
-		# 1. Ta bort fönsterramar
+		# GÅ TILL FULLSCREEN
 		DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true, main_id)
-		# 2. Maximera fönstret så det täcker allt (inklusive Taskbar)
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED, main_id)
+		
+		# SKRIK I MEGAFONEN!
+		window_mode_changed.emit(true)
 	else:
-		# GÅ TILLBAKA TILL FÖNSTER
-		# 1. Sätt tillbaka fönsterramar
+		# GÅ TILL FÖNSTERLÄGE
 		DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false, main_id)
-		# 2. Sätt tillbaka till vanligt fönsterläge
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED, main_id)
 		
-		# 3. TVINGA NER STORLEKEN (viktigt för jobbdatorn!)
-		# Vi sätter den till 1600x900 så vi vet att den ryms på 1080p med skalning
 		await get_tree().process_frame
 		DisplayServer.window_set_size(Vector2i(1600, 900), main_id)
 		
-		# 4. Centrera
 		var screen_rect = DisplayServer.screen_get_usable_rect()
 		var center_pos = screen_rect.position + (screen_rect.size / 2) - (Vector2i(1600*0.5, 900*0.5))
 		DisplayServer.window_set_position(center_pos, main_id)
-
-	print("Bytte läge! Nytt läge: ", DisplayServer.window_get_mode(main_id))
+		
+		# SKRIK I MEGAFONEN!
+		window_mode_changed.emit(false)
---
+++ uno-ultimate-ai-simulator/menus/full_screen_button.gd
@@ -0,0 +1,23 @@
+extends Button
+
+func _ready():
+	# 1. Lyssna på när knappen klickas
+	pressed.connect(_on_pressed)
+	
+	# 2. Lyssna på när F11 trycks via din Autoload (Byt ut 'GlobalInputs' mot vad din Autoload heter i inställningarna!)
+	GlobalInputs.window_mode_changed.connect(_update_text)
+	
+	# 3. Sätt rätt text direkt när knappen laddas in
+	var main_id = DisplayServer.MAIN_WINDOW_ID
+	var is_full = DisplayServer.window_get_mode(main_id) != DisplayServer.WINDOW_MODE_WINDOWED
+	_update_text(is_full)
+
+func _on_pressed():
+	# När vi klickar, säg åt Autoloaden att göra sitt jobb!
+	GlobalInputs.toggle_window_mode()
+
+func _update_text(is_fullscreen: bool):
+	if is_fullscreen:
+		text = "Windowed (F11)"
+	else:
+		text = "Fullscreen (F11)"
